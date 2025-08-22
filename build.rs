039f51c81da5fcfaf71a017dfb822bae8bbef3da
hev <git@hev.cc>
2025-08-22 19:26:05 +0800

    Build: Fix link libraries.

--- build.rs
+++ build.rs
@@ -10,4 +10,10 @@ fn main() {
 
     println!("cargo:rustc-link-search=native=impl/bin");
     println!("cargo:rustc-link-lib=static=hev-socks5-tunnel");
+    println!("cargo:rustc-link-search=native=impl/third-part/hev-task-system/bin");
+    println!("cargo:rustc-link-lib=static=hev-task-system");
+    println!("cargo:rustc-link-search=native=impl/third-part/lwip/bin");
+    println!("cargo:rustc-link-lib=static=lwip");
+    println!("cargo:rustc-link-search=native=impl/third-part/yaml/bin");
+    println!("cargo:rustc-link-lib=static=yaml");
 }
